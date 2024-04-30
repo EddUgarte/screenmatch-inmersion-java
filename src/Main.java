b2f033d8b2da8ac6243a7cc1351d99900c1cabb5
EddUgarte <edd.ugarte7@gmail.com>
2024-04-30 12:51:34 -0600

    aplicacion del desafio aula 01

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        String nombrePelicula = "Matrix";
+        int fechaLanzamiento = 1992;
+        double evaluacionUsuario = 0;
+        int numEvaluaciones = 3;
+        int contador = 0;
+        System.out.println("SCREEN MATCH");
+        System.out.println("Nombre de la pelicula: " + nombrePelicula);
+        System.out.println("Fecha de lanzamiento: " + fechaLanzamiento);
 
-        System.out.println("Hello world!");
+        while(contador < numEvaluaciones) {
+            System.out.println("Ingresa la evaluación " + (contador+1) + "/" + numEvaluaciones + ":");
+            evaluacionUsuario = evaluacionUsuario + leer.nextDouble();
+            contador += 1;
+        }
+
+        System.out.println("La media de evaluaciones es: " + (evaluacionUsuario/numEvaluaciones));
+
+
+
 
 
     }
